cd8bd731f328a2140450e09fea224d6cfa71aaa2
mohamed adel <mmohamed@sure.com.sa>
2019-11-10 17:12:15 +0200

    work on register page

--- client/src/components/registration/signIn.vue
+++ client/src/components/registration/signIn.vue
@@ -9,7 +9,5 @@ export default {};
 </script>
 
 <style lang="scss">
-section {
-    color: $primary
-}
+
 </style>
--- client/src/views/registration.vue
+++ client/src/views/registration.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <section id="registration">
-        <router-view/>
+        <div id="reg-container" class="d-flex justify-content-center align-items-center">
+            <div class="block col-lg-4 col-md-6 col-sm-8 col-11 rounded">
+                <router-view/>
+            </div>
+        </div>
     </section>
   </main>
 </template>
@@ -13,5 +17,21 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+#registration {
+    background-image: linear-gradient(rgba(240, 240, 240, 0.39), rgba(0, 0, 0, 0.801)),
+    url('https://backgrounddownload.com/wp-content/uploads/2018/09/underwater-city-background-8.jpg');
+    height: 722px;
+    width: 100%;
+    background-size: cover;
+    margin: 0;
+    #reg-container {
+        height: 100%;
+        .block {
+            background: $transDarkBlue;
+            height: 80%;
+            
+        }
+    }
+}
 </style>
