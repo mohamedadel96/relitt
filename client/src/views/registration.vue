0bcf47ac8b7476112810a4c0fec67d82b4a97608
mohamed adel <mmohamed@sure.com.sa>
2019-11-10 17:25:32 +0200

    work on register page

--- client/src/views/registration.vue
+++ client/src/views/registration.vue
@@ -1,37 +1,39 @@
 <template>
   <main>
     <section id="registration">
-        <div id="reg-container" class="d-flex justify-content-center align-items-center">
-            <div class="block col-lg-4 col-md-6 col-sm-8 col-11 mt-5">
-                <router-view/>
-            </div>
+      <div id="reg-container" class="d-flex justify-content-center align-items-center">
+        <div class="block col-lg-4 col-md-6 col-sm-8 col-11 mt-5 d-flex justify-content-center align-items-center">
+          <div class="col-9">
+            <router-view />
+          </div>
         </div>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
 #registration {
-    background-image: linear-gradient(rgba(240, 240, 240, 0.39), rgba(0, 0, 0, 0.801)),
-    url('https://backgrounddownload.com/wp-content/uploads/2018/09/underwater-city-background-8.jpg');
-    height: 722px;
-    width: 100%;
-    background-size: cover;
-    margin: 0;
-    #reg-container {
-        height: 100%;
-        .block {
-            background: $transDarkBlue;
-            height: 82%;
-            border-radius: 12px;
-        }
+  background-image: linear-gradient(
+      rgba(240, 240, 240, 0.39),
+      rgba(0, 0, 0, 0.801)
+    ),
+    url("https://backgrounddownload.com/wp-content/uploads/2018/09/underwater-city-background-8.jpg");
+  height: 722px;
+  width: 100%;
+  background-size: cover;
+  margin: 0;
+  #reg-container {
+    height: 100%;
+    .block {
+      background: $transDarkBlue;
+      height: 82%;
+      border-radius: 12px;
     }
+  }
 }
 </style>
