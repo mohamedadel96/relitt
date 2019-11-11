c1e85c53acd01964327951c9081137b30ea58696
mohamed adel <mmohamed@sure.com.sa>
2019-11-11 18:05:27 +0200

    work on home page

---
+++ client/src/components/home/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+      <nav-bar/>
+  </header>
+</template>
+
+<script>
+import navBar from './navbar'
+export default {
+    components: {
+        navBar
+    }
+};
+</script>
+
+<style lang="scss">
+    header {
+        background: #888
+    }
+</style>
--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-
+    <app-header />
   </div>
 </template>
 
 <script>
+import appHeader from '../components/home/header'
 
 export default {
-
+components: {
+  appHeader
+}
 }
 </script>
 
