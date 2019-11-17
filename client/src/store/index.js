3ac3a0b172e6a9cfc16ba5e6b2feda33de943b23
mohamed adel <mmohamed@sure.com.sa>
2019-11-17 16:48:52 +0200

    edits

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import signup from './auth/signup'
 
 Vue.use(Vuex)
 
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   actions: {
   },
   modules: {
+    signup: signup
   }
 })
