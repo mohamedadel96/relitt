5e6d2477880036870eb856a5f6909a8ec32a5e35
ziadbastawy <50346363+ziadbastawy@users.noreply.github.com>
2019-11-24 22:31:40 +0200

    Add files via upload

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
 export default () => {
+
+  const token = localStorage.getItem("token")
   return axios.create({
     baseURL: 'http://34.253.188.105/relitt/public/api/client/',
     headers: {
-      Authorization: '',
-      "Content-Type": "application/json"
+      Authorization: `Bearer + ${token}`
     }
-  })
-}
+  });
+};
+
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -13,7 +13,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     user: JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')) : null,
-    token: JSON.parse(localStorage.getItem('user')) ? (JSON.parse(localStorage.getItem('user'))).token : null
+    token: JSON.parse(localStorage.getItem('token')) ? (JSON.parse(localStorage.getItem('token'))) : null
   },
   getters: {
     getUser(state) {
@@ -27,6 +27,7 @@ export default new Vuex.Store({
     saveAuthData(state, data) {
       localStorage.setItem('user', JSON.stringify(data.data))
       state.user = data.data
+      localStorage.setItem('token', JSON.stringify(data.data.token))
       state.token = data.data.token
     }
   },
