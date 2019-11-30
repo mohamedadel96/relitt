cc23e0419981fbc62674c887fa626d758edffcf0
Mohamed Adel <mohamed7adel96@gmail.com>
2019-11-30 17:41:20 +0200

    edits

--- client/src/views/sub-views/event.vue
+++ client/src/views/sub-views/event.vue
@@ -33,9 +33,6 @@ export default {
 
 <style lang="scss" scoped>
 #event {
-  background-color: #fafafb;
-  color: #00223c;
-
   img {
     width: 100%;
     height: 370px;
@@ -43,7 +40,6 @@ export default {
   }
 
   .container-fluid {
-    // padding: 60px 65px;
     background-color: #fff;
     -webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
     -moz-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
