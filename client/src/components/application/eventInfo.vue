41b2ad72970a7ae766c853f91a2b75a55242733b
Mohamed Adel <mohamed7adel96@gmail.com>
2019-11-30 16:02:05 +0200

    add event card

---
+++ client/src/components/application/eventInfo.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <div>
+      <h3>From Citadel To Qitbay To Al Montazah Walking Tour</h3>
+      <div class="d-flex justify-conent-between mt-4 mb-5">
+        <div class="col-6 pl-0">
+          <button class="btn btn-primary btn-block">Joined</button>
+        </div>
+        <div class="col-6 pr-0">
+          <button class="btn btn-outline-primary btn-block">Invite</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-between py-2">
+      <div class="col-6 px-0 d-flex">
+        <p class="mr-3">icon</p>
+        <p>19/12/2019</p>
+      </div>
+      <div class="col-6 px-0 d-flex">
+        <p class="mr-3">icon</p>
+        <p>19/12/2019</p>
+      </div>
+    </div>
+
+    <div class="d-flex border-top border-bottom py-2">
+      <p class="mr-3">icon</p>
+      <p>address here</p>
+    </div>
+
+    <div class="pt-1 pb-2 border-bottom d-flex">
+      <div class="d-flex">
+        <p class="mr-3">icon</p>
+      </div>
+      <div>
+        <p>address of description</p>
+        <p>description</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ client/src/views/sub-views/event.vue
@@ -0,0 +1,50 @@
+<template>
+  <section id="event">
+    <div class="backgroundimg">
+      <img src="../../assets/img/Layer1.png" />
+    </div>
+    <div class="container-fluid col-10 p-5">
+      <event-info />
+      <attendance />
+      <review />
+    </div>
+  </section>
+</template>
+
+<script>
+import review from "../../components/application/review";
+import eventInfo from "../../components/application/eventInfo";
+import attendance from "../../components/application/attendance";
+
+export default {
+  components: {
+    review,
+    eventInfo,
+    attendance
+  },
+  mounted() {
+    this.$store.dispatch("EVENT", this.$route.params.id);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#event {
+  background-color: #fafafb;
+  color: #00223c;
+
+  img {
+    width: 100%;
+    height: 370px;
+    object-fit: cover;
+  }
+
+  .container-fluid {
+    // padding: 60px 65px;
+    background-color: #fff;
+    -webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+    -moz-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+  }
+}
+</style>
