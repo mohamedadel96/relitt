6d875070842e9863c04f717026535d502c195490
Mohamed Adel <mohamed7adel96@gmail.com>
2019-12-11 23:27:20 +0200

    edits

--- client/src/components/application/feeds.vue
+++ client/src/components/application/feeds.vue
@@ -1,14 +1,30 @@
 <template>
   <section id="feeds">
-    <div class="search rounded py-1 d-flex justify-content-between align-items-center">
-      <input class="py-2 px-4" placeholder="what's in your mind" type="text" />
-      <div class="d-flex align-items-center">
-        <span class="px-0">
-          <img src="../../assets/img/icon/path2.png" alt />
-        </span>
-        <span class="px-3">
-          <img src="../../assets/img/icon/path.svg" alt />
-        </span>
+    <div class="create-post rounded py-1">
+      <div class="post-media">
+        <img class="m-3" src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt />
+      </div>
+      <div class="post-body d-flex justify-content-between align-items-start">
+        <textarea @focus="postBtn = true" class="py-2 px-4" placeholder="what's in your mind"></textarea>
+
+        <div
+          :class="['d-flex flex-wrap align-items-between justify-content-end ', { 'enlargement' : postBtn}]"
+        >
+          <div class="col-12 px-0 d-flex align-items-start justify-content-end pt-2">
+            <span class="px-0">
+              <img class="pointer" src="../../assets/img/icon/path2.png" alt />
+            </span>
+            <span class="px-3">
+              <img class="pointer" src="../../assets/img/icon/path.svg" alt />
+            </span>
+          </div>
+          <div
+            :class="['col-12 px-3 pb-2 postBtn', {'d-flex align-items-end justify-content-end showBtn' : postBtn}]"
+            v-show="postBtn"
+          >
+            <button class="btn btn-primary">post</button>
+          </div>
+        </div>
       </div>
     </div>
 
@@ -28,11 +44,12 @@
           </div>
           <div>
             <span>
-              <!-- <img
+              <img
+                class="pointer"
                 src="../../assets/img/icon/Icon - Share - Filled.svg"
                 style="width:24px ; height:24px"
                 alt
-              /> -->
+              />
             </span>
           </div>
         </div>
@@ -96,9 +113,13 @@
 
         <hr class="border mx-3 my-1" />
 
-        <div class=" d-flex col-12 px-0 text-center ">
-          <div class="col-6 py-2 px-0 post-options fontSM font-weight-bold"><img src="../../assets/img/icon/Icon - Thumbs Up - Dark.png" class="mr-1 mb-1"> Like</div>
-          <div class="col-6 py-2 px-0 post-options fontCS"><img src="../../assets/img/icon/round-comment-24px.png" class="mr-1 ">Comment</div>
+        <div class="d-flex col-12 px-0 text-center">
+          <div class="col-6 py-2 px-0 post-options fontSM font-weight-bold">
+            <img src="../../assets/img/icon/Icon - Thumbs Up - Dark.png" class="mr-1 mb-1" /> Like
+          </div>
+          <div class="col-6 py-2 px-0 post-options fontCS">
+            <img src="../../assets/img/icon/round-comment-24px.png" class="mr-1" />Comment
+          </div>
         </div>
       </div>
     </div>
@@ -109,6 +130,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      postBtn: false
+    };
+  },
   computed: {
     feeds() {
       return this.$store.getters.feeds;
@@ -130,14 +156,56 @@ export default {
 
 <style lang="scss" scoped>
 #feeds {
-  .search {
+  .create-post {
     background: $background_white;
     box-shadow: 1px 1px 4px #ddd;
+    position: relative;
 
-    input {
-      background: none;
-      border: none;
-      outline: none;
+    .post-media {
+      overflow: hidden;
+      img {
+        width: 200px;
+        height: 200px;
+        object-fit: cover;
+      }
+    }
+
+    .post-body {
+      textarea {
+        background: none;
+        border: none;
+        outline: none;
+        height: 40px;
+        width: 98%;
+        resize: none;
+        transition: 0.4s;
+        &:focus {
+          height: 250px;
+        }
+      }
+
+      .enlargement {
+        -webkit-animation-name: elongation; /* Safari 4.0 - 8.0 */
+        -webkit-animation-duration: 2s; /* Safari 4.0 - 8.0 */
+        animation-name: elongation;
+        animation-duration: 2s;
+        height: 250px;
+      }
+
+      .postBtn {
+        position: absolute;
+        height: 100;
+        bottom: 0;
+        opacity: 0;
+      }
+
+      .showBtn {
+        -webkit-animation-name: showBtn; /* Safari 4.0 - 8.0 */
+        -webkit-animation-duration: 2s; /* Safari 4.0 - 8.0 */
+        animation-name: showBtn;
+        animation-duration: 2s;
+        opacity: 1;
+      }
     }
   }
 
@@ -210,11 +278,48 @@ export default {
           opacity: 0.6;
         }
       }
-       
+
       .post-options {
         color: $gray;
         opacity: 0.7;
       }
+    }
+  }
+  @-webkit-keyframes elongation {
+    from {
+      height: auto;
+    }
+    to {
+      height: 250px;
+    }
+  }
+
+  /* Standard syntax */
+  @keyframes elongation {
+    from {
+      height: auto;
+    }
+    to {
+      height: 250px;
+    }
+  }
+
+  @-webkit-keyframes showBtn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  /* Standard syntax */
+  @keyframes showBtn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
     }
   }
 }
