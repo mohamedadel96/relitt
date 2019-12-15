3f6a84af5fc52172de9231ce1697b7153bc9ad00
ziadbastawy <ziadmahmoud413@gmail.com>
2019-12-15 23:56:37 +0200

    upload-file

--- client/src/components/application/events/createEvent.vue
+++ client/src/components/application/events/createEvent.vue
@@ -13,16 +13,26 @@
             <input type="text" class="form-control" placeholder="Title" v-model="form.title" />
           </div>
           <div class="form-group">
-            <input type="date" class="form-control" placeholder="Start Date" v-model="form.start_date"  />
+            <input
+              type="date"
+              class="form-control"
+              placeholder="Start Date"
+              v-model="form.start_date"
+            />
           </div>
           <div class="form-group">
-            <input type="date" class="form-control" placeholder="End Date" v-model="form.end_date"  />
+            <input type="date" class="form-control" placeholder="End Date" v-model="form.end_date" />
           </div>
           <!-- <div class="form-group">
             <input type="text" class="form-control" placeholder="Location" v-model="form.location" />
-          </div> -->
+          </div>-->
           <div class="form-group">
-            <textarea class="form-control" id="message-text" placeholder="Description" v-model="form.description"></textarea>
+            <textarea
+              class="form-control"
+              id="message-text"
+              placeholder="Description"
+              v-model="form.description"
+            ></textarea>
           </div>
           <div class="form-group">
             <input
@@ -30,7 +40,18 @@
               class="form-control-file"
               id="exampleFormControlFile1"
               placeholder="Add Photo"
+              ref="pic"
+              @change="uploadFiles('pic')"
+              name="pic"
+              accept="image/*"
+              multiple
             />
+            <img 
+            @click="$refs.pic.click()"
+              class="pointer"
+              src="../../../assets/img/icon/path.svg"
+              alt
+              >
           </div>
           <button type="button" class="btn btn-primary" @click="create()">Create</button>
         </form>
@@ -42,27 +63,47 @@
 export default {
   data() {
     return {
-      form:{
-        title:null,
-        description:null,
-        start_date:null,
-        end_date:null,
-        images:["null"]
+      form: {
+        title: null,
+        description: null,
+        start_date: null,
+        end_date: null,
+        images: ["null"]
       }
-  };
+    };
   },
-  methods:{
-    async create(){
-            try {
-              const data = this.form
+  methods: {
+    async create() {
+      try {
+        const data = this.form;
         let res = await this.$store.dispatch("CREATEEVENT", this.form);
       } catch (error) {
         console.log(error);
       }
+    },
+    uploadFiles(ref) {
+      try {
+        this.disablePost = true;
+        let formData = new FormData();
 
+        for (let i = 0; i < this.$refs[ref].files.length; i++) {
+          let file = this.$refs[ref].files[i];
+          formData.append("files[" + i + "]", file);
+        }
+
+        this.$store.dispatch("UPLOADFILES", formData).then(res => {
+          this.$toasted.success("uploaded successfully");
+          res.map(file => {
+            this.form.images.push(file.filePath);
+          });
+
+          this.disablePost = false;
+        });
+      } catch (error) {
+        this.$toasted.error("error while uploading files");
+      }
     }
   }
-  
 };
 </script>
 
