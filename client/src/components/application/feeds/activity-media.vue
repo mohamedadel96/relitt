dc0bac0f33f9dc5473c1b32fe57f574debe20d40
mohamed adel <mmohamed@sure.com.sa>
2019-12-24 16:24:13 +0200

    add static code

--- client/src/components/application/feeds/activity-media.vue
+++ client/src/components/application/feeds/activity-media.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="post-media mb-3">
     <div :class="['st-pic', activity.type_id === 1 ? 'divingBG' : 'scubaBG']">
-
+      <div class="data-container position-relative">
+        <div class="temprature text-white fontXS font-weight-bold">24 &#8451;</div>
+        <div class="time text-white fontXS font-weight-bold">24 min 12 sec</div>
+        
+        <div>
+          <div class="col-12 d-flex justify-content-center flex-wrap mb-2">
+            <img src="../../../assets/icons/line.svg" alt />
+          </div>
+          <div class="col-12 d-flex justify-content-center flex-wrap">
+            <div class="position-relative">
+              <img v-if="activity.type_id === 1" src="../../../assets/icons/diving.svg" alt />
+              <img v-if="activity.type_id === 2" src="../../../assets/icons/scuba-diving.svg" alt />
+              <span class="text-white fontXS font-weight-bold position-absolute px-1 text-nowrap">50 m</span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <!-- <img class="st-pic col-12 px-0" v-if="activity.length" :src="activity[0].url" alt="post picture" />
     <div class="pics d-flex" v-if="activity.length - 1 > 0">
@@ -35,14 +51,28 @@ export default {
 .post-media {
   .st-pic {
     height: 230px;
-    &.scubaBG {
-      background: url("../../../assets/img/scuba-diving.jpg");
-      background-size: cover;
+    .data-container {
+        height: 100%;
+      .time,
+      .temprature {
+        position: absolute;
+        right: 7px;
+        &.time {
+          bottom: 5px;
+        }
+        &.temprature {
+          top: 5px;
+        }
+      }
     }
-    &.divingBG {
-      background: url("../../../assets/img/diving.jpg");
-      background-size: cover;
-    }
+      &.scubaBG {
+        background: url("../../../assets/img/scuba-diving.jpg");
+        background-size: cover;
+      }
+      &.divingBG {
+        background: url("../../../assets/img/diving.jpg");
+        background-size: cover;
+      }
   }
 
   .pics {
