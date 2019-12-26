434bf2e6a2000d27239e5793edbd288367914868
mohamed adel <mmohamed@sure.com.sa>
2019-12-26 10:09:59 +0200

    edits

--- client/src/main.js
+++ client/src/main.js
@@ -2,23 +2,21 @@ import Vue from "vue";
 import store from "./store";
 import router from './router'
 import App from "./App.vue";
-import Vuelidate from "vuelidate";
-import TextareaAutosize from "vue-textarea-autosize";
 import Aos from "aos";
 import './plugins/routerGuard.js'
 import './plugins/bootstrap.js'
+import './plugins/moment.js'
 import './plugins/flatPickr.js'
 import './plugins/vueScrollTo.js'
 import './plugins/infiniteLoading.js'
 import './plugins/toasted.js'
+import './plugins/textareaAutosize.js'
+import './plugins/vuelidation.js'
 import "aos/dist/aos.css";
 
 import SweetModal from "sweet-modal-vue/src/plugin.js";
 
 Vue.use(SweetModal);
-Vue.use(TextareaAutosize);
-Vue.use(require("vue-moment"));
-Vue.use(Vuelidate);
 Vue.config.productionTip = false;
 
 new Vue({
