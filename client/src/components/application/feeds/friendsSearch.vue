517cadaafab654f8d482872f11ee19b163833f17
mohamed adel <mmohamed@sure.com.sa>
2020-01-02 17:37:52 +0200

    edits

--- client/src/components/application/feeds/friendsSearch.vue
+++ client/src/components/application/feeds/friendsSearch.vue
@@ -1,66 +1,32 @@
 <template>
   <section id="friendsSearch">
-    <p class="title font-weight-bold fontSM">Find friends</p>
-    <div class="search mb-3 d-flex align-items-center">
-      <span class="px-4"><img src="../../../assets/img/icon/ic_search.png" alt=""></span>
-      <input placeholder="Search people" class="py-3" type="text" />
-    </div>
-    <div class="friends mt-2" v-for="i in 3" :key="i">
-      <div class="mb-4">
-        <div class="d-flex align-items-center mb-2">
-          <img
-            class="border rounded-circle mr-3"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-            alt="friend profile picture"
-          />
-          <div>
-            <p class="name font-weight-bold fontSM">Ahmed reda</p>
-            <p class="mb-0 job fontCS">Senior diver</p>
-          </div>
-        </div>
-        <div>
-          <button class="btn btn-outline-primary btn-block fontXS">Follow</button>
-        </div>
-        <hr class="border" />
+    <div class="search-sec pl-4 pr-5 pt-4 col-12 px-0 rounded">
+      <p class="title font-weight-bold fontSM">Find friends</p>
+      <div class="search mb-3 d-flex align-items-center">
+        <span class="px-4">
+          <img src="../../../assets/img/icon/ic_search.png" alt />
+        </span>
+        <input placeholder="Search people" class="py-3" type="text" />
       </div>
-      <!-- del -->
-
-      <!--<div class="mb-4">
-        <div class="d-flex align-items-center mb-2">
-          <img
-            class="border rounded-circle mr-3"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-            alt="friend profile picture"
-          />
-          <div>
-            <p class="name font-weight-bold">Ahmed reda</p>
-            <p class="mb-0 job">Senior diver</p>
+      <div class="friends mt-2" v-for="i in 3" :key="i">
+        <div class="mb-4">
+          <div class="d-flex align-items-center mb-2">
+            <img
+              class="border rounded-circle mr-3"
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+              alt="friend profile picture"
+            />
+            <div>
+              <p class="name font-weight-bold fontSM">Ahmed reda</p>
+              <p class="mb-0 job fontCS">Senior diver</p>
+            </div>
           </div>
-        </div>
-        <div>
-          <button class="btn btn-outline-primary btn-block">Follow</button>
-        </div>
-        <hr class="border" />
-      </div>
-
-      <div class="mb-4">
-        <div class="d-flex align-items-center mb-2">
-          <img
-            class="border rounded-circle mr-3"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-            alt="friend profile picture"
-          />
           <div>
-            <p class="name font-weight-bold">Ahmed reda</p>
-            <p class="mb-0 job">Senior diver</p>
+            <button class="btn btn-outline-primary btn-block fontXS">Follow</button>
           </div>
+          <hr class="border" />
         </div>
-        <div>
-          <button class="btn btn-outline-primary btn-block">Follow</button>
-        </div>
-        <hr class="border" />
       </div>
-       del -->
     </div>
   </section>
 </template>
@@ -71,6 +37,10 @@ export default {};
 
 <style lang="scss" scoped>
 #friendsSearch {
+  .search-sec {
+    background: $background_white;
+    box-shadow: 1px 1px 4px #ddd;
+  }
   .search {
     background: #f8f8ff;
     border-radius: 11px;
--- client/src/views/sub-views/app-layout.vue
+++ client/src/views/sub-views/app-layout.vue
@@ -3,16 +3,14 @@
     <div class="left-side col-md-3 d-md-block d-none px-4 position-fixed">
       <div class="px-1">
         <user-card />
-        <side-menu/>
+        <side-menu />
       </div>
     </div>
     <div class="col-md-6 col-12">
       <router-view />
     </div>
     <div class="col-md-3 d-md-block d-none px-3">
-     <div class="search-sec pl-4 pr-5 pt-4 col-12 px-0 rounded">
-        <friends-search />
-      </div>
+      <friends-search />
       <myEvent />
     </div>
   </section>
@@ -24,7 +22,6 @@ import sideMenu from "../../components/application/feeds/side-menu/sideMenu";
 import expCard from "../../components/application/dashboard/expCard";
 import friendsSearch from "../../components/application/feeds/friendsSearch";
 import myEvent from "../../components/application/events/myEvent";
-
 
 export default {
   components: {
@@ -40,11 +37,8 @@ export default {
 <style lang="scss" scoped>
 #app-layout {
   .left-side {
-    left: 0
+    left: 0;
   }
-  .search-sec {
-    background: $background_white;
-    box-shadow: 1px 1px 4px #ddd;
-  }
+
 }
 </style>
