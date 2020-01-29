9fe0ee880f659bec52685df03b5f81d6a6baf558
mohamed adel <mmohamed@sure.com.sa>
2020-01-29 19:20:50 +0200

    edits

--- client/src/components/application/feeds/comments.vue
+++ client/src/components/application/feeds/comments.vue
@@ -3,55 +3,69 @@
     <div class="card" v-for="(comment , i) in comments" :key="i">
       <div class="d-flex">
         <div>
-          <img :src="comment.user.image ? comment.user.image : require('../../../assets/img/default-avatar.jpg')" class="card-img" alt="user image" />
+          <img
+            :src="comment.user.image ? comment.user.image : require('../../../assets/img/default-avatar.jpg')"
+            class="card-img"
+            alt="user image"
+          />
         </div>
         <div class="col-11 px-1">
           <div class="card-body px-2">
             <div class="d-flex justify-content-between">
               <p
-                class="card-title m-0 d-inline-block"
+                class="card-title d-inline-block"
               >{{comment.user.firstname}} {{comment.user.lastname}}</p>
-              <!-- <p class="card-text m-0 font-12">stars</p> -->
+              <div v-if="comment.canEdit">
+                <b-dropdown
+                  size="lg"
+                  variant="link"
+                  right
+                  offset="-20"
+                  toggle-class="text-decoration-none"
+                  no-caret
+                >
+                  <template v-slot:button-content>
+                    <span>
+                      <img
+                        class="pointer mb-0"
+                        src="../../../assets/icons/3dots.svg"
+                        style="width:24px ; height:24px"
+                        alt="options"
+                      />
+                    </span>
+                  </template>
+                  <b-dropdown-item @click="editState = i">Edit Comment</b-dropdown-item>
+                  <b-dropdown-item @click="deleteComment(comment)">Delete Comment</b-dropdown-item>
+                </b-dropdown>
+              </div>
             </div>
-            <p class="card-text font-12 px-1 mb-1">{{comment.updated_at | moment("from", "now")}}</p>
             <div>
               <textarea-autosize
                 placeholder="Type something here..."
                 :disabled="editState !== i"
                 :ref="'commentBody_' + i"
-                :class="['comment-body col-12 px-2', {'editable': editState === i}]"
+                :class="['comment-body col-11 px-2 bg-light border rounded', {'editable': editState === i}]"
                 :value="comment.body"
                 :min-height="30"
                 :max-height="350"
                 @input="(e)=> form.body = e"
               />
               <div class="d-flex justify-content-end" v-if="comment.canEdit">
-                <h5 v-show="editState !== i">
+                <p v-show="editState === i">
                   <b-badge
                     variant="light"
-                    class="pointer px-3 mx-2 font-weight-light text-primary"
-                    @click="editState = i"
-                  >Edit</b-badge>
-                  <b-badge
-                    variant="light"
-                    class="pointer px-3 font-weight-light text-danger"
-                    @click="deleteComment(comment)"
-                  >Delete</b-badge>
-                </h5>
-                <h5 v-show="editState === i">
-                  <b-badge
-                    variant="light"
-                    class="pointer px-3 mx-2 font-weight-light text-danger"
+                    class="pointer px-3 mx-2 font-14 font-weight-light text-danger"
                     @click="cancelEditMode(comment)"
                   >Cancel</b-badge>
                   <b-badge
                     variant="light"
-                    class="pointer px-3 mx-2 font-weight-light text-success"
+                    class="pointer px-3 mx-2 font-14 font-weight-light text-success"
                     @click="editComment(comment.id)"
                   >Save</b-badge>
-                </h5>
+                </p>
               </div>
             </div>
+            <p class="card-text col-11 font-12 px-1 mb-1 text-right">{{comment.updated_at | moment("from", "now")}}</p>
           </div>
         </div>
       </div>
@@ -111,7 +125,7 @@ export default {
   .card {
     border: none;
 
-    img {
+    .card-img {
       width: 50px;
       height: 50px;
       border-radius: 50%;
