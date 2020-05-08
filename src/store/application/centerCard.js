e024392e86f96e58872bec9b79c63c64e2a6ca46
mohamed adel <mmohamed@sure.com.sa>
2020-05-08 20:27:11 +0200

    replace folder outside client

---
+++ src/store/application/centerCard.js
@@ -0,0 +1,64 @@
+import appServices from '../../services/application'
+
+export default {
+  state: {
+    centercard: [],
+    myReviews: [],
+    filter: {
+      page: 1
+    }
+
+  },
+  getters: {
+    getCenterCard(state) {
+      return state.centercard
+    },
+    myReviews(state) {
+      return state.myReviews
+    }
+  },
+  mutations: {
+    saveCenterCard(state, data) {
+      state.filter.page += 1
+      state.centercard.push(...data)
+    },
+    saveMyReviews(state, data) {
+      state.myReviews = data
+    }
+  },
+  actions: {
+    CENTERCARD({ commit, state, dispatch }) {
+      return new Promise((resolve, reject) => {
+        appServices.centerCard(state.filter).then(res => {
+          if (res.data.status === 401) {
+            // we will handle logout option // call logout function
+            dispatch("LOGOUT")
+          }
+          if (res.data.code !== 200) return reject(res.data.message)
+          if (!res.data.data.length) { return resolve('end') }
+
+          commit('saveCenterCard', res.data.data)
+          resolve('done')
+        })
+      })
+    },
+    MYREVIEWS({ commit , dispatch }) {
+      return new Promise((resolve, reject) => {
+        appServices.myReviews().then(res => {
+          if (res.data.status === 401) {
+            // we will handle logout option // call logout function
+            dispatch("LOGOUT")
+
+          }
+          if (res.data.code !== 200) return reject(res.data.message)
+
+          commit('saveMyReviews', res.data.data)
+          resolve('done')
+        })
+      })
+    },
+    CENTERFILTER({}, data){
+      console.log(data)
+    }
+  }
+}
---
+++ src/views/sub-views/center.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="center" v-if="center">
+    <!-- <div class="backgroundimg">
+      <img src="../../assets/img/Layer1.png" />
+    </div>-->
+    <div class="container-fluid col-lg-10 col-12 px-0">
+      <center-carsoule :images="center.images" />
+    </div>
+    <div class="container-fluid col-lg-10 col-12 py-3 px-md-5 px-3">
+      <centerInfo :elementData="center"></centerInfo>
+      <review :elementData="center"></review>
+      <centerComment :elementData="center"></centerComment>
+      <location :lat="center.lat" :lng="center.lng"></location>
+    </div>
+    <app-footer />
+  </div>
+</template>
+
+<script>
+import review from "../../components/application/centers/review";
+import location from "../../components/application/centers/location";
+import centerComment from "../../components/application/centers/centerComment";
+import centerInfo from "../../components/application/centers/centerInfo";
+import centerCarsoule from "../../components/application/centers/centerCarsoule";
+import appFooter from "../../components/application/global/footer";
+
+export default {
+  components: {
+    centerCarsoule,
+    review,
+    location,
+    centerComment,
+    centerInfo,
+    appFooter
+  },
+  computed: {
+    center() {
+      return this.$store.getters.center;
+    }
+  },
+  created() {
+    this.$store.dispatch("CENTER", this.$route.params.id);
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#center {
+  background-color: #fafafb;
+  color: #00223c;
+
+  img {
+    width: 100%;
+    height: 30%;
+  }
+
+  .container-fluid {
+    background-color: #fff;
+    -webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+    -moz-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+    box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.05);
+
+    h2 {
+      margin: 20px;
+    }
+
+    hr {
+      margin: 10px 0px;
+    }
+
+    p {
+      margin: 10px 20px;
+    }
+    .service img {
+      width: 50px;
+      height: 50px;
+    }
+  }
+}
+</style>
